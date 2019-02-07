3e9c94922c4a74e0c10de072a0b89ac8c1071480
Katie Mangan <katie.mangan1@gmail.com>
2019-02-07 09:52:36 -0600

    append wolves array to DOM

--- server/public/client.js
+++ server/public/client.js
@@ -1,5 +1,17 @@
 $(document).ready(onReady);
 
-function onReady(){
-    console.log('hi');
+function onReady() {
+    $.ajax({
+        type: 'GET',
+        url: '/wolves'
+    }).then(function (response) {
+        for (let i = 0; i < response.length; i++) {
+            $('#wolvesList').append(`
+                <ul>
+                    <li>${response[i].commonName}</li>
+                    <li>${response[i].scientificName}</li>
+                </ul>
+            `);
+        }
+    });
 }
--- server/server.js
+++ server/server.js
@@ -4,6 +4,12 @@ const PORT = 5000;
 
 app.use(express.static('server/public'));
 
+let wolves = require('./modules/wolves');
+
+app.get('/wolves', (req, res) => {
+    res.send(wolves);
+}); 
+
 app.listen(PORT, () => {
     console.log('listening on port', PORT)
 });
